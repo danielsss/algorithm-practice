77788032f550149f8d1e0c397dc7b08856e7deb0
danielsss <better.sunjian@gmail.com>
2022-11-27 20:55:23 +1100

    added: binary heap

--- src/heap/heap.ts
+++ src/heap/heap.ts
@@ -1,3 +1,31 @@
-class Heap {}
+import { Comparator } from '../utils';
+
+class Heap extends Comparator {
+
+  /**
+   * Get index of parent
+   * @param i
+   */
+  public parent(i: number) {
+    return i / 2;
+  }
+
+  /**
+   * Get index of left child
+   * @param i
+   */
+  public left(i: number) {
+    return i * 2;
+  }
+
+
+  /**
+   * Get index of right child
+   * @param i
+   */
+  public right(i: number) {
+    return i * 2 + 1;
+  }
+}
 
 export default Heap;
