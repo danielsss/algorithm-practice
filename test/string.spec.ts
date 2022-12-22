3ffc3d40f5c0895effd238426541b8b027f9d906
danielsss <better.sunjian@gmail.com>
2022-12-22 13:02:05 +1100

    added: more test case

--- test/string.spec.ts
+++ test/string.spec.ts
@@ -7,9 +7,9 @@ describe('- String algorithms test case', function() {
   it('# 20. Valid Parentheses', done => {
     const sa = new StringAlgorithm();
     expect(sa.isValidParentheses('()[]{}')).to.be.true;
-    // expect(sa.isValidParentheses('()')).to.be.true;
-    // expect(sa.isValidParentheses('(]')).to.be.false;
-    // expect(sa.isValidParentheses('[({})]')).to.be.true;
+    expect(sa.isValidParentheses('()')).to.be.true;
+    expect(sa.isValidParentheses('(]')).to.be.false;
+    expect(sa.isValidParentheses('[({})]')).to.be.true;
     done(null);
   });
 
